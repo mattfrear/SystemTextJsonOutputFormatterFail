ba1639ae24c54e95d443e0b1701171fa635a444b
Matt Frear <matt.frear@trustpower.co.nz>
2020-06-24 14:23:48 +1200

    Added Newtonsoft and System.Text.Json test

---
+++ SystemTextJsonOutputFormatterFail/MvcOutputFormatter.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.Net.Http.Headers;
+using System;
+using System.IO;
+
+namespace SystemTextJsonOutputFormatterFail
+{
+    internal class MvcOutputFormatter
+    {
+        public string Serialize<T>(T value, IOutputFormatter formatter, MediaTypeHeaderValue contentType)
+        {
+            if (value == null)
+            {
+                return string.Empty;
+            }
+
+            using (var stringWriter = new StringWriter())
+            {
+                var outputFormatterContext = GetOutputFormatterContext(
+                    stringWriter,
+                    value,
+                    value.GetType(),
+                    contentType);
+
+                formatter.WriteAsync(outputFormatterContext).GetAwaiter().GetResult();
+                stringWriter.FlushAsync().GetAwaiter().GetResult();
+                return stringWriter.ToString();
+            }
+        }
+
+        private static OutputFormatterWriteContext GetOutputFormatterContext(
+            TextWriter writer,
+            object outputValue,
+            Type outputType,
+            MediaTypeHeaderValue contentType)
+        {
+            return new OutputFormatterWriteContext(
+                GetHttpContext(contentType),
+                (stream, encoding) => writer,
+                outputType,
+                outputValue);
+        }
+
+        private static HttpContext GetHttpContext(MediaTypeHeaderValue contentType)
+        {
+            var httpContext = new DefaultHttpContext();
+
+            httpContext.Request.Headers[HeaderNames.AcceptCharset] = contentType.Charset.ToString();
+            httpContext.Request.Headers[HeaderNames.Accept] = contentType.MediaType.Value;
+            httpContext.Request.ContentType = contentType.MediaType.Value;
+
+            httpContext.Response.Body = new MemoryStream();
+            httpContext.RequestServices =
+                new ServiceCollection()
+                    .AddSingleton(Options.Create(new MvcOptions()))
+                    .BuildServiceProvider();
+
+            return httpContext;
+        }
+    }
+}
--- SystemTextJsonOutputFormatterFail/Program.cs
+++ SystemTextJsonOutputFormatterFail/Program.cs
@@ -1,18 +1,13 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Formatters;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
 using Microsoft.Net.Http.Headers;
 using Models;
 using System;
-using System.Collections.Generic;
-using System.IO;
+using System.Buffers;
 
 namespace SystemTextJsonOutputFormatterFail
 {
-    static class Program
+    class Program
     {
         private static readonly MediaTypeHeaderValue ApplicationXml = MediaTypeHeaderValue.Parse("application/xml; charset=utf-8");
         private static readonly MediaTypeHeaderValue ApplicationJson = MediaTypeHeaderValue.Parse("application/json; charset=utf-8");
@@ -21,72 +16,26 @@ namespace SystemTextJsonOutputFormatterFail
         {
             var person = new Person { Title = Title.Mr, FirstName = "Matt", Age = 99, Income = 1234 };
 
-            var xmlResult = Serialize(person, ApplicationXml);
+            var mvcOutputFormatter = new MvcOutputFormatter();
 
-            Console.WriteLine("Xml:" + xmlResult);
+            var xmlFormatter = new XmlDataContractSerializerOutputFormatter();
+            var xmlResult = mvcOutputFormatter.Serialize(person, xmlFormatter, ApplicationXml);
+            Console.WriteLine("Xml: " + xmlResult);
+
+            var newtonsoftFormatter = new NewtonsoftJsonOutputFormatter(
+                new Newtonsoft.Json.JsonSerializerSettings(),
+                ArrayPool<char>.Shared,
+                new MvcOptions());
+
+            var newtonSoftJsonResult = mvcOutputFormatter.Serialize(person, newtonsoftFormatter, ApplicationJson);
+            Console.WriteLine("Newtonsoft Json: " + newtonSoftJsonResult);
+
+            var systemTextJsonFormatter = new SystemTextJsonOutputFormatter(new System.Text.Json.JsonSerializerOptions());
+            var systemTextJsonResult = mvcOutputFormatter.Serialize(person, systemTextJsonFormatter, ApplicationJson);
+
+            Console.WriteLine("System.Text.Json: " + systemTextJsonResult);
+
+            Console.ReadLine();
         }
-
-        private static string Serialize(Person value, MediaTypeHeaderValue contentType)
-        {
-            using (var stringWriter = new StringWriter())
-            {
-                var outputFormatterContext = GetOutputFormatterContext(
-                    stringWriter,
-                    value,
-                    value.GetType(),
-                    contentType);
-
-                //var formatter = OutputFormatterSelector.SelectFormatter(
-                //    outputFormatterContext,
-                //    new List<IOutputFormatter>(),
-                //    new MediaTypeCollection());
-
-                //if (formatter == null)
-                //{
-                //    throw new FormatterNotFoundException(contentType);
-                //}
-
-                //formatter.WriteAsync(outputFormatterContext).GetAwaiter().GetResult();
-                //stringWriter.FlushAsync().GetAwaiter().GetResult();
-                return stringWriter.ToString();
-            }
-        }
-
-        private static OutputFormatterWriteContext GetOutputFormatterContext(
-            TextWriter writer,
-            object outputValue,
-            Type outputType,
-            MediaTypeHeaderValue contentType)
-        {
-            return new OutputFormatterWriteContext(
-                GetHttpContext(contentType),
-                (stream, encoding) => writer,
-                outputType,
-                outputValue);
-        }
-
-        private static HttpContext GetHttpContext(MediaTypeHeaderValue contentType)
-        {
-            var httpContext = new DefaultHttpContext();
-
-            httpContext.Request.Headers[HeaderNames.AcceptCharset] = contentType.Charset.ToString();
-            httpContext.Request.Headers[HeaderNames.Accept] = contentType.MediaType.Value;
-            httpContext.Request.ContentType = contentType.MediaType.Value;
-
-            httpContext.Response.Body = new MemoryStream();
-            httpContext.RequestServices =
-                new ServiceCollection()
-                    .AddSingleton(Options.Create(new MvcOptions()))
-                    .BuildServiceProvider();
-
-            return httpContext;
-        }
-    }
-
-    internal class FormatterNotFoundException : Exception
-    {
-        public FormatterNotFoundException(MediaTypeHeaderValue contentType)
-            : base($"OutputFormatter not found for '{contentType}'")
-        { }
     }
 }
